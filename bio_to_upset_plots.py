485e73ef1822ed5bcbc7ff630a543c49c2779f2f
rundimeco <rundimeco@gmail.com>
2021-07-09 21:16:15 +0200

    verify tokens

--- bio_to_upset_plots.py
+++ bio_to_upset_plots.py
@@ -5,9 +5,8 @@ import re
 
 def file2_triple(path):
   """ Prepares BIO data"""
-  f = open(path)
-  l = f.readlines()
-  f.close()
+  with open(path) as f:
+    l = f.readlines()
   res= [re.split(" ", re.sub("\n", "", x)) for x in l if len(x)>2]
   return [x for x in res if len(x)==3]
 import itertools
@@ -20,17 +19,17 @@ def get_combination(names):
       liste_possible.append(tuple(sorted(list(subset))))
   return liste_possible
 
-def bio2upsetData(data, names):
+def bio2upsetData(data, names, debug = False):
   """Transforms BIO data in upset_plot format"""
   liste_possible = get_combination(names)
   print(  "%i possible combinations"%len(liste_possible))
   all_res = {}
+  stats = {"lines":len(data[0])}
   for i in range(len(data[0])):
 
     truth = "P" #by default
     if data[0][i][2]=="O":
-      truth = "N" #else it is a negative
-
+      truth = "N" #else it is a positive
     pred = {"T":[], "F":[]}#to store True and False predictions
     for j in range(len(names)):
       if data[j][i][1]==data[j][i][2]:
@@ -76,12 +75,27 @@ def plot_graph(res, path):
   write_json_file(path_upset, [liste_cats, data_out])
   print(f"  output file in upset plot format stored in '{path_upset}'")
 
+def verify_data(liste_files, data_in):
+  """Verify the number of BIO lines and that the tokens are the same"""
+  for i in range(1, len(data_in)):
+    f0 = liste_files[0]
+    fi = liste_files[i]
+    l0 = len(data_in[0])
+    li = len(data_in[i])
+    assert l0==li, f"Inconsistent number of BIO lines :\n {f0} ({l0}) VS {fi} ({li})"
+    for j in range(len(data_in[0])):
+      tok0 = data_in[0][j][0]
+      toki = data_in[i][j][0]
+      assert tok0==toki, f"Not the same token line {j} : \n f{0} --> {tok0}\n f{1} --> {toki}" 
+
 def files_2_cat(path):
   liste_files = glob.glob(f"{path}/*.tx*")
   data_in = [file2_triple(path_file) for path_file in liste_files]
+  verify_data(liste_files, data_in)
+
   names = [re.split("/|\.", x)[-2] for x in liste_files]
   print(f"\nfilenames : {names}")
-  res = bio2upsetData(data_in, names)
+  res = bio2upsetData(data_in, names, debug=DEBUG)
   plot_graph(res, path)
 
 
@@ -93,12 +107,14 @@ if __name__=="__main__":
   print("Usage : python bio_to_upset_plots.py DIR")
   print("DIR is the directory where the aligned result files (BIO format) are stored")
   print("The names displayed on the graphs will be derived from the filenames")
-  
-  if len(sys.argv)!=2:
+  DEBUG = False
+  if len(sys.argv)==1:
     print("\nspecify the path of the directory containg the BIO result files")
     print("\nfor example python bio_to_upset_plots.py example_data")
     print("...exiting ...")
     exit()
-  
+  elif len(sys.argv)==3:
+    print("--- debug mode ---")
+    DEBUG = True
   path = sys.argv[1]
   files_2_cat(path)
